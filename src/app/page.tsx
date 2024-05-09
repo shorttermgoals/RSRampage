0c3554ab275e9b31fa4696605e82bc6ea9316ac4
shorttermgoals <hectorgg2002@gmail.com>
2024-05-09 16:18:57 +0200

    shooting mechanic working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import React, { useState } from "react";
 
 export default function Home() {
+
+  const [clickCoordinate, setClickCoordinate] = useState<{x: number, y: number, bulletNo: number}[]>([]);
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+
+    const x = event.clientX;
+    const y = event.clientY;
+    const bulletNo = bulletNumberRandomize();
+
+    console.log(x, y);
+
+    setClickCoordinate(prevCoordinates => [...prevCoordinates, { x, y, bulletNo }]);
+  };
+
+  const bulletNumberRandomize = () => {
+    return Math.floor(Math.random() * 7) + 1;
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <div className="app-container" onClick={handleClick}>
+        {/* No renderizar imágenes antes del primer clic */}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      {/* Renderizar los elementos de imagen solo cuando se hace clic */}
+      {clickCoordinate.map(({ x, y, bulletNo }, index) => (
+        <img
+          key={index}
+          src={`/bullets/bullet-hole${bulletNo}.png`}
+          alt="Imagen"
+          style={{ 
+            position: 'absolute',
+            top: y,
+            left: x,
+            transform: 'translate(-50%,-50%)',
+            userSelect: 'none', 
+          }}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ))}
     </main>
   );
 }
