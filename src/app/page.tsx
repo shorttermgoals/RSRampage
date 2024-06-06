5ad5df313245004ec96020a853bd87faa2d9e735
shorttermgoals <hectorgg2002@gmail.com>
2024-06-07 00:52:11 +0200

    Items placed after head text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,18 @@ export default function Home() {
         </div>
         <div className="under-title">
           <div className="url">
-            <a>made by shorttermgoalz</a>
+            <span className="url-text">made by <a className="url-text" href="">shorttermgoalz</a></span>
           </div>
+        </div>
+        <div className="image-background">
+          <div className="image">
+            <img className="valla" src="/images/Valla.png"></img>
+            <Car></Car>
+          </div>
+          
+        </div>
+        <div className="reset-button">
+          <div className="button"></div>
         </div>
       </div>
     </main>
